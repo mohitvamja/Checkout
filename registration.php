886c1365bed4fd1a68121d84acee5e7a7ee64bfc
mohitvamja <mohitvamja1991@gmail.com>
2022-02-16 20:18:46 +0530

    add file

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Sale_Paymenttext',
+    'Sale_Paymenttext123',
     __DIR__
 );
